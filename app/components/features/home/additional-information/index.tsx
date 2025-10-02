6366bde13a29a7c369562a0e9d66cc52acd7f809
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-10-02 14:13:21 -0600

    responsive para redes sociales y ubicación en contador

--- app/components/features/home/additional-information/index.tsx
+++ app/components/features/home/additional-information/index.tsx
@@ -4,22 +4,24 @@ import InstagramIcon from "@/components/icons/instagram";
 import LocationIcon from "@/components/icons/location";
 import TiktokIcon from "@/components/icons/tiktok";
 
+import styles from "./additional-information.module.scss";
+
 export default function AdditionalInformation() {
   return (
-    <div className="m-auto mt-5 flex w-fit items-center justify-center text-white max-xl:flex-col max-xl:gap-3 xl:mt-10">
-      <div className="flex items-center gap-2 border-b-2 border-solid border-b-white max-xl:justify-center max-xl:pb-2 max-lg:justify-between xl:border-r-[4px] xl:border-r-white xl:pr-10">
+    <div className={styles["additional-information"]}>
+      <div className={styles["location-container"]}>
         <SocialNetworkButton
           icon={<LocationIcon />}
           href="https://maps.app.goo.gl/oSj8djKsvYvHtuj89"
         />
         <div className="flex flex-col font-[Creato_Display] text-xl">
-          <span className="font-light">Plaza</span>
+          <span className="font-light tracking-wider">Plaza</span>
           <span className="mt-[-7.5px] font-bold tracking-wider">
             Presidente
           </span>
         </div>
       </div>
-      <div className="flex w-full max-xl:justify-between max-lg:px-1.5 xl:gap-3 xl:pl-10">
+      <div className={styles["social-networks"]}>
         <SocialNetworkButton
           icon={<InstagramIcon />}
           href="https://www.instagram.com/portafoliodg.udb?igsh=MThvamZiNXRoejVlbg=="
--- app/components/features/home/countdown/item.tsx
+++ app/components/features/home/countdown/item.tsx
@@ -1,7 +1,7 @@
 export default function CountdownItem({ number, label }: CountdownItemProps) {
   return (
     <div className="flex flex-col font-[Nexa] text-white xl:text-center">
-      <span className="text-7xl font-bold max-xl:border-b-2 xl:text-9xl">
+      <span className="text-7xl font-bold max-xl:border-b-2 md:text-8xl">
         {number}
       </span>
       <span className="text-3xl font-extralight xl:text-4xl">{label}</span>
