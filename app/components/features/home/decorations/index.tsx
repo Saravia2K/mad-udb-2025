1fee17e167948d039057201d1a85a713acd08adb
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-10-02 23:09:45 -0600

    decoraciones laterales (circulo y cuadrado)

--- app/components/features/home/decorations/index.tsx
+++ app/components/features/home/decorations/index.tsx
@@ -1,19 +1,15 @@
 import { Fragment, type PropsWithChildren } from "react";
 import clsx from "clsx";
-import { DesktopTriangle } from "./triangle";
 
 import styles from "./decorations.module.scss";
 
 export default function Decorations({ children }: PropsWithChildren) {
   return (
     <Fragment>
-      {/* <div className={clsx(styles.decoration, styles["top-left-circle"])}></div>
+      <div className={clsx(styles.decoration, styles["top-left-circle"])}></div>
       <div
         className={clsx(styles.decoration, styles["bottom-right-square"])}
-      ></div> */}
-      {/* <DesktopTriangle
-        className={clsx(styles.decoration, styles["bottom-right-triangle"])}
-      /> */}
+      ></div>
       {children}
     </Fragment>
   );
