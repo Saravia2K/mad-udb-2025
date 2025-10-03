ab673f69a119d6e8947a9bcda82a91d02dd9033c
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-10-03 09:14:41 -0600

    añadiéndole más ancho al chip

--- app/components/features/home/date-chip/index.tsx
+++ app/components/features/home/date-chip/index.tsx
@@ -2,7 +2,7 @@ import "./date-chip.css";
 
 export default function DateChip() {
   return (
-    <div className="date-chip w-[60%] rounded-full py-[1rem] text-2xl sm:w-[40%] lg:w-[25%] xl:text-[2.3rem] 2xl:w-[15%] 2xl:py-[1.5rem] 2xl:text-5xl">
+    <div className="date-chip w-[60%] rounded-full py-[1rem] text-2xl sm:w-[40%] lg:w-[25%] xl:text-[2.3rem] 2xl:w-[20rem] 2xl:py-[1.5rem] 2xl:text-5xl">
       13.Oct.2025
     </div>
   );
