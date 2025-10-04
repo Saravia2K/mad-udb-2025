db758a230ca0be315fa79045e651b847233aafc9
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-10-04 10:16:40 -0600

    nueva versión de additional-information

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,7 +4,6 @@ import Main from "@/components/features/home/main";
 import Countdown from "@/components/features/home/countdown";
 import DateChip from "@/components/features/home/date-chip";
 import AdditionalInformation from "@/components/features/home/additional-information";
-import Decorations from "@/components/features/home/decorations";
 
 import logoMAD from "@/assets/images/logo_mad.svg";
 
@@ -18,10 +17,11 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <Main>
-      <div className="relative flex size-full flex-col justify-center max-lg:px-48 max-md:px-[20vw] lg:items-center">
+      <div className="relative flex size-full flex-col items-center justify-center max-lg:px-48 max-md:px-[20vw]">
         <img src={logoMAD} alt="MAD - PRISMA/DSGN" className="lg:w-[20rem]" />
         <Countdown />
         <DateChip />
+        <AdditionalInformation />
       </div>
     </Main>
   );
