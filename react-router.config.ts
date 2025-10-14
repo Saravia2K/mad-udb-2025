f9ceba0a2e54b906a5ad914f7309a91e05b4326d
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-10-13 22:00:23 -0600

    pre-renderizando todos los perfiles

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from "@react-router/dev/config";
+import PROFILES from "./app/assets/json/profiles.json";
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  prerender: ["/proximamente"],
+  prerender: ["/proximamente", ...PROFILES.map((p) => `/${p.slug}`)],
 } satisfies Config;
